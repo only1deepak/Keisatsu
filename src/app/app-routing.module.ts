6b1d69b8286482e3c81d5bb7d4289eb7934452e7
only1deepak <53404785+only1deepak@users.noreply.github.com>
2020-07-24 23:10:24 +0530

    modified buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'public',
+    loadChildren: () => import('./public/public.module').then( m => m.PublicPageModule)
+  },
   
 ];
 

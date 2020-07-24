544a23f6f55bed0dc493b6665553150feeab6562
only1deepak <53404785+only1deepak@users.noreply.github.com>
2020-07-24 20:33:15 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,15 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -22,6 +25,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      
     });
   }
 }

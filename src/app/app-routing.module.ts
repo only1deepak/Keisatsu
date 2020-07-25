00b06c6c455932d953fa03e19251a7554dc73fb4
only1deepak <53404785+only1deepak@users.noreply.github.com>
2020-07-25 19:43:44 +0530

    appointment page edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,31 +5,19 @@ const routes: Routes =[
   
   {
     path: '',
-    redirectTo: 'public-fir',
+    redirectTo: 'public-appointment',
     pathMatch: 'full'
   },
-  {
-    path: 'public-fir',
-    loadChildren: () => import('./public-fir/public-fir.module').then( m => m.PublicFIRPageModule)
-  },
+
   {
     path: 'public-appointment',
     loadChildren: () => import('./public-appointment/public-appointment.module').then( m => m.PublicAppointmentPageModule)
   },
-  {
-    path: 'public-noc',
-    loadChildren: () => import('./public-noc/public-noc.module').then( m => m.PublicNOCPageModule)
-  },
-  {
-    path:'public-home',
-    loadChildren: () => import('./public-home/public-home.module').then( m => m.PublicHomePageModule)
-  }
-
-
 
   
+
   
-];
+ ];
 
 @NgModule({
   imports: [

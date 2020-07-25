6592be7bd7b64edbd5db3119042611e0f1e2e656
Oshine <oshine1603@yahoo.in>
2020-07-25 16:44:41 +0530

    changed buttons in fir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-<<<<<<< HEAD
-  {
+const routes: Routes =[
   
-
+  {
     path: '',
     redirectTo: 'public-fir',
     pathMatch: 'full'
@@ -15,18 +13,6 @@ const routes: Routes = [
     loadChildren: () => import('./public-fir/public-fir.module').then( m => m.PublicFIRPageModule)
   }
 
-=======
-  
-  {
-    path: '',
-    redirectTo: 'public-home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'public-home',
-    loadChildren: () => import('./public-home/public-home.module').then( m => m.PublicHomePageModule)
-  },
->>>>>>> a2095c09c0cd2c141c8ff8a3ba218203cd388faf
   
   
 ];

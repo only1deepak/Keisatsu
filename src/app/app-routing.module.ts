b0553a08d6da88d23a4c72603654c68e112ba24b
Oshine <oshine1603@yahoo.in>
2020-07-26 14:53:22 +0530

    added admin main page n few tweaks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,7 +42,11 @@ const routes: Routes =[
   {
     path: 'police-main',
     loadChildren: () => import('./police-main/police-main.module').then( m => m.PoliceMainPageModule)
+  },  {
+    path: 'admin-main',
+    loadChildren: () => import('./admin-main/admin-main.module').then( m => m.AdminMainPageModule)
   }
+
 ];
 
 @NgModule({
